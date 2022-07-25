929a8375b4185a52864a7c4fb6c3f100c90ed77d
brendobrendo <brendan.smith903@gmail.com>
2022-07-25 11:02:59 -0700

    Messages style and read access for messages

--- screens/Messages.js
+++ screens/Messages.js
@@ -1,18 +1,31 @@
-import { Modal, Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { query, collection, getDocs } from 'firebase/firestore/lite';
+import { db } from '../firebase';
 
 const Messages = (props) => {
 
+    const getData = async () => {
+        
+        const messagesCol = await getDocs(collection(db, 'messages'));
+        const messagesList = messagesCol.docs.map(doc => doc.data());
+
+        console.log(messagesList);
+    }
+
     return (
-        <Modal visible={props.visible} animationType="slide">
+        <Modal visible={props.visible} animationType="slide" style={styles.container}>
             <View>
                 <Text>Messages</Text>
             </View>
             <View>
                 <Text>Hello World</Text>
             </View>
-            <Pressable onPress={props.closeMessages}>
-                <Text>Close messages</Text>
+            <Pressable onPress={props.closeMessages} style={styles.button}>
+                <Text style={styles.buttonText}>Close messages</Text>
+            </Pressable>
+            <Pressable onPress={getData} style={styles.button}>
+                <Text style={styles.buttonText}>Get Data</Text>
             </Pressable>
         </Modal>
     )
@@ -20,4 +33,23 @@ const Messages = (props) => {
 
 export default Messages
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      button: {
+        backgroundColor: '#0782F9',
+        width: '60%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 40,
+      },
+      buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
+      },
+})
