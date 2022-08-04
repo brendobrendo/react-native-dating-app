aa384853d5bb3402f8a1359506138cfc2dbaa2c4
brendobrendo <brendan.smith903@gmail.com>
2022-08-04 06:34:22 -0700

    Chat message formatting and break from location

--- screens/Messages.js
+++ screens/Messages.js
@@ -2,18 +2,14 @@ import { Modal, Pressable, StyleSheet, Text, View, FlatList, TextInput, Keyboard
 import React, { useState, useLayoutEffect } from 'react';
 import { collection, addDoc, Timestamp, query, orderBy, limit, onSnapshot, where, getDocs } from 'firebase/firestore';
 import { db, authentication } from '../firebase';
-import config from '../config'
 import ChatMessage from './components/ChatMessage';
-import LocationOption from './LocationOption';
-import * as Location from 'expo-location'
-import axios from 'axios';
+import Header from './components/Header';
+import Footer from './components/Footer'
 
 const Messages = (props) => {
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState("");
-    const [userLocation, setUserLocation] = useState({});
-    const [modalIsVisible, setModalIsVisible] = useState(false);
-    const [locationsInfo, setLocationsInfo] = useState([{'user_ratings_total': null, 'price_level': null, 'rating': null, 'name': null, 'photos': [{'photo_reference': null}] }]);
+
 
     useLayoutEffect(() => {
         const q = query(collection(db, "messages"), orderBy('createdAt', 'desc'), limit(5))
@@ -21,7 +17,7 @@ const Messages = (props) => {
         const unsubscribe = onSnapshot(q, (snapshot) => {
             let new5messages = [];
             snapshot.forEach((doc) => {
-                new5messages.push({'text': doc.data().text})
+                new5messages.push(doc.data());
             })
             setMessages(new5messages);
             // setMessages({last5messages: new5messages});
@@ -34,7 +30,8 @@ const Messages = (props) => {
             const docRef = addDoc(collection(db, "messages"), {
                 senderId: authentication.currentUser.uid,
                 text: newMessage,
-                createdAt: Timestamp.now()
+                createdAt: Timestamp.now(),
+                recipientId: "KLUDoUpCZoRommxPFUp4qKr6nZM2"
             })
             console.log("Document written with ID: ", docRef.id);
             setNewMessage("")
@@ -44,35 +41,18 @@ const Messages = (props) => {
         }
     }
 
-    const locationHandler = async () => {
-        // code to grab location, expo-location
-        const location = await (async () => {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
-                return;
-            }
-            let location = await Location.getCurrentPositionAsync({});
-            console.log(location.coords.latitude, location.coords.longitude);
-            setUserLocation(location);
-        })();
-    };
-
-    const getNearbyLocations = () => {
-        axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLocation.coords.latitude}%2C${userLocation.coords.longitude}&radius=1000&type=restaurant&key=${config.GOOGLE_PLACES_API_KEY}`)
-            .then(response => setLocationsInfo(response.data.results));
-    }
-
-    const startLocationModal = () => {
-        setModalIsVisible(true);
-    };
-
-    const endLocationModal = () => {
-        setModalIsVisible(false);
-    };
-
     return (
         <Modal visible={props.visible} animationType="slide" style={styles.container}>
+            <Header />
+            <View style={styles.messagesContainer}>
+                <FlatList data={messages}
+                renderItem={(msgData) => {
+                    return (
+                        <ChatMessage messageText={msgData.item} />
+                    )
+                }}
+            />
+            </View>
             <KeyboardAvoidingView behavior="padding">
             <View style={styles.buttonContainer}>
                 <Pressable onPress={props.closeMessages} style={styles.button}>
@@ -87,27 +67,9 @@ const Messages = (props) => {
                 <Pressable onPress={handleNewMessage} style={styles.button}>
                     <Text style={styles.buttonText}>Add Message</Text>
                 </Pressable>
-                <Pressable onPress={locationHandler} style={styles.geoButton}>
-                    <Text style={styles.buttonText}>Get Geo Location</Text>
-                </Pressable>
-                <Pressable onPress={getNearbyLocations} style={styles.geoButton}>
-                    <Text style={styles.buttonText}>Get local joints</Text>
-                </Pressable>
-                <Pressable onPress={startLocationModal} style={styles.geoButton}>
-                    <Text style={styles.buttonText}>Photos</Text>
-                </Pressable>
             </View>
             </KeyboardAvoidingView>
-            <LocationOption closeModal={endLocationModal} showModal={modalIsVisible} placesInfo={locationsInfo}/>
-            <View style={styles.messagesContainer}>
-                <FlatList data={messages}
-                renderItem={(msgData) => {
-                    return (
-                        <ChatMessage messageText={msgData.item.text} />
-                    )
-                }}
-            />
-            </View>
+            <Footer />
         </Modal>
     )
 }
--- screens/components/ChatMessage.js
+++ screens/components/ChatMessage.js
@@ -1,12 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { authentication } from '../../firebase';
 
 const ChatMessage = (props) => {
-  
+  const getMessageBoxStyle = () => {
+    if (props.messageText.senderId === authentication.currentUser.uid) {
+      console.log('chat message', props.messageText.senderId)
+      return ({
+          backgroundColor: '#0782F9',
+          padding: 10,
+          borderRadius: 10,
+          marginVertical: 10,
+          marginLeft: 10,
+          width: '80%',
+      }) 
+      } else return ({
+        backgroundColor: 'gray',
+        padding: 10,
+        borderRadius: 10,
+        marginVertical: 10,
+        marginLeft: 10,
+        width: '80%',
+        })
+      }
 
   return (
-    <View style={styles.messageBox}>
-      <Text style={styles.messageBoxText}>{props.messageText}</Text>
+    <View style = {props.messageText.senderId === authentication.currentUser.uid ? styles.currentUserAlignment : styles.otherUserAlignment} >
+    <View style={props.messageText.senderId === authentication.currentUser.uid ? styles.currentUserMessageBox: styles.otherUserMessageBox}>
+      <Text style={props.messageText.senderId === authentication.currentUser.uid ? styles.currentUserMessageBoxText: styles.otherUserMessageBoxText}>{props.messageText.text}</Text>
+    </View>
     </View>
   )
 }
@@ -14,15 +36,30 @@ const ChatMessage = (props) => {
 export default ChatMessage
 
 const styles = StyleSheet.create({
-    messageBox: {
+  currentUserAlignment: {
+    alignItems: 'flex-end'
+  },
+  otherUserAlignment: {
+    alignItems: 'flex-start'
+  },
+  currentUserMessageBox: {
         backgroundColor: '#0782F9',
         padding: 10,
         borderRadius: 10,
-        marginVertical: 10,
-        marginLeft: 10,
+        margin: 10,
         width: '80%',
     },
-    messageBoxText: {
+    currentUserMessageBoxText: {
         color: 'white',
-    }
+    },
+    otherUserMessageBox: {
+      backgroundColor: '#ebecf0',
+      padding: 10,
+      borderRadius: 10,
+      margin: 10,
+      width: '80%',
+  },
+  otherUserMessageBoxText: {
+      color: 'black',
+  }
 })
